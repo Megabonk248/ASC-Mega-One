4c4b459a95b946cab9623f75383eae0dca065ddb
Megabonk248 <e12104977@student.tuwien.ac.at>
2025-11-12 16:57:25 +0100

    add simd file

---
+++ demos/test_simd_functions.cpp
@@ -0,0 +1,8 @@
+#include<simd_functions.hpp>
+
+namespace bla = ASC_bla;
+
+int main()
+{
+    return 0;
+}
---
+++ src/simd_functions.hpp
@@ -0,0 +1,10 @@
+#ifndef FILE_SIMD_FUNCTIONS
+#define FILE_SIMD_FUNCTIONS
+
+#include <iostream>
+
+namespace ASC_bla
+{
+
+}
+#endif
